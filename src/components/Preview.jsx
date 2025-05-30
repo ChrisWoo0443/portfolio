6e1f11a617b7ee6579a7256ef2a6511f297996cf
ChrisWoo0443 <chefwoodini@gmail.com>
2025-05-30 16:25:39 -0700

    finished showcase

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import largeImage from "../images/JobGPT_home.png";
+import smallImage1 from "../images/finger_paint.png";
+import smallImage2 from "../images/natural_query.png";
+import smallImage3 from "../images/sms.png";
 
 export function Preview() {
+    const [mainImage, setMainImage] = useState(largeImage);
+    const [thumbnails, setThumbnails] = useState([
+        smallImage1,
+        smallImage2,
+        smallImage3,
+    ]);
+
+    const handleImageSwap = (index) => {
+        const newThumbnails = [...thumbnails];
+        const clickedImage = newThumbnails[index];
+
+        // Swap the clicked image with the main image
+        newThumbnails[index] = mainImage;
+        setMainImage(clickedImage);
+        setThumbnails(newThumbnails);
+    };
+
     return (
         <div className="preview-container">
-            <img src={largeImage} alt="JobGPT Home" className="preview-large"/>
+            <img src={mainImage} alt="Main Preview" className="preview-large" />
+
+            <div className="preview-small">
+                {thumbnails.map((img, index) => (
+                    <div className="image-wrapper" key={index}>
+                        <img
+                            src={img}
+                            alt={`Preview ${index}`}
+                            className="preview-small-image"
+                            onClick={() => handleImageSwap(index)}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
