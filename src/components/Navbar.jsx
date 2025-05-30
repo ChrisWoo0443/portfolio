7160fa79b4b17f45be1bbcb802c6c065b0ce85a8
ChrisWoo0443 <chefwoodini@gmail.com>
2025-05-30 14:36:31 -0700

    removed resume and changed order

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,9 @@ export function Navbar(){
             <a href="/" className="navbar-logo">Chris Woo</a>
             <div className="navbar-items">
                 <a href="#home" className='navbar-link'>Home</a>
-                <a href="#about" className='navbar-link'>About</a>
                 <a href="#projects" className='navbar-link'>Projects</a>
+                <a href="#about" className='navbar-link'>About</a>
                 <a href="#contact" className='navbar-link'>Contact</a>
-                <a href="#resume" className='navbar-link'>Resume</a>
             </div>
         </nav>
     );
