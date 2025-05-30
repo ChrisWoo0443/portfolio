f4197e010f0f21b856a9cdefe53d14d91ad11a24
ChrisWoo0443 <chefwoodini@gmail.com>
2025-05-30 00:53:41 -0700

    moved images outside of components

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Preview() {
     return (
         <div className="preview-container">
-            {/* <img src={require("/images/JobGPT_home.png")} alt="JobGPT Home"/> */}
+            {/* <img src={require("./images/JobGPT_home.png")} alt="JobGPT Home"/> */}
         </div>
     );
 }
