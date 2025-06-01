7264b6f5a8870ba5740046397836ce146f558054
ChrisWoo0443 <chefwoodini@gmail.com>
2025-05-31 19:35:00 -0700

    changed navbar button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export function Navbar(){
     return (
         <nav className="navbar">
-            <a href="/" className="navbar-logo">Chris Woo</a>
+            <a href="/portfolio/" className="navbar-logo">Chris Woo</a>
             <div className="navbar-items">
                 <a href="/projects" className='navbar-link'>Projects</a>
                 <a href="#about" className='navbar-link'>About</a>
